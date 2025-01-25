e9c36e5e0f33060db58e815fafeeee4b0c05f28d
kasperab <kasperab@yahoo.se>
2025-01-25 20:47:32 +0100

    Random movement for bubble in BubblePop

--- Bubbleware/Assets/Scripts/MiniGames/BubblePop/PopBubble.cs
+++ Bubbleware/Assets/Scripts/MiniGames/BubblePop/PopBubble.cs
@@ -8,9 +8,12 @@ public class PopBubble : MonoBehaviour
     public float minY;
     public float maxY;
     public float popRange;
+    public float minSwitchTime;
+    public float maxSwitchTime;
 
     private float direction;
     private Vector3 startPostition;
+    private float switchCountdown;
 
     private void Awake()
     {
@@ -22,24 +25,38 @@ public class PopBubble : MonoBehaviour
         direction = 1;
         transform.position = startPostition;
         StartCoroutine(CloseTheGameAsync(10));
+        RandomizeSwitchCountdown();
     }
 
     private void Update()
     {
+        switchCountdown -= Time.deltaTime;
+        if (switchCountdown <= 0)
+        {
+            RandomizeSwitchCountdown();
+            direction = -direction;
+        }
         transform.Translate(0, direction * speed * Time.deltaTime, 0);
         if (transform.position.y < minY)
         {
             direction = 1;
+            RandomizeSwitchCountdown();
         }
         else if (transform.position.y > maxY)
         {
             direction = -1;
+            RandomizeSwitchCountdown();
         }
     }
 
     public bool IsPoppable()
     {
         return math.abs(0 - transform.position.y) < popRange;
+    }
+
+    private void RandomizeSwitchCountdown()
+    {
+        switchCountdown = UnityEngine.Random.Range(minSwitchTime, maxSwitchTime);
     }
 
     private IEnumerator CloseTheGameAsync(float delay)
