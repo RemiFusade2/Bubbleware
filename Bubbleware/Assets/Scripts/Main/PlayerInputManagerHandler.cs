9df7fd75acd5e8318a21168e75810677e4dfa1a3
Remi Fusade <remi.fusade@gmail.com>
2025-01-25 13:40:50 +0100

    Made PlayerInputManagerHandler as a singleton that does not get disabled
    Signed-off-by: Remi Fusade <remi.fusade@gmail.com>

--- Bubbleware/Assets/Scripts/Main/PlayerInputManagerHandler.cs
+++ Bubbleware/Assets/Scripts/Main/PlayerInputManagerHandler.cs
@@ -3,9 +3,26 @@ using UnityEngine.InputSystem;
 
 public class PlayerInputManagerHandler : MonoBehaviour
 {
+    public static PlayerInputManagerHandler Instance;
+
     private void OnEnable()
     {
         GetComponent<PlayerInputManager>().EnableJoining();
+    }
+
+    private void Awake()
+    {
+        // Does another instance already exist?
+        if (Instance && Instance != this)
+        {
+            // Destroy myself
+            Destroy(gameObject);
+            return;
+        }
+
+        // Otherwise store my reference and make me DontDestroyOnLoad
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
